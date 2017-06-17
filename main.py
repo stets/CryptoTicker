a14c48234d990104af2d635c8c45a6eb29a0fd26
Stetson Blake <stetsblake@gmail.com>
2017-06-17 17:41:38 -0400

    Started playing with forms

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import ticker
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email
 
 app = Flask(__name__)
+
+class LoginForm(Form):
+    qty = StringField('username', validators=[InputRequired(), Email()])
+    password = StringField('password', validators=[InputRequired()])
 
 
 @app.route('/')
 def root():
     price, symbol = ticker.getPrice() 
     return render_template('index.html', price=price, symbol=symbol)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    form = LoginForm()
+
 
 if __name__ == '__main__':
     app.run()
